41a28b31d7848b4cebe43e3aee0b76986bef6409
aleroza <aleroza1910@gmail.com>
2026-04-06 23:12:03 +0300

    актуализация

--- configuration.nix
+++ configuration.nix
@@ -136,7 +136,7 @@
   };
 
   environment.shellAliases = {
-    ll = "ls -l";
+    ll = "ls -lah";
     nix-rebuild = "sudo nixos-rebuild switch --flake .#aleroza-pc";
     nix-gen = ''echo "Path: $(readlink /run/current-system)"; echo "  ID: $(readlink /nix/var/nix/profiles/system)"'';
   };
@@ -155,7 +155,23 @@
 
   nixpkgs.config.allowUnfree = true;
 
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+  };
+
   programs.firefox.enable = true;
+
+  services.flatpak.enable = true;
+
+  system.activationScripts = {
+    flatpak-setup = ''
+      ${pkgs.flatpak}/bin/flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+      ${pkgs.flatpak}/bin/flatpak install --noninteractive flathub com.github.tchx84.Flatseal
+    '';
+  };
 
   virtualisation.docker.enable = true;
   virtualisation.docker.autoPrune.enable = true;
@@ -164,17 +180,27 @@
   # You can use https://search.nixos.org/ to find more packages (and options).
   environment.systemPackages = with pkgs; [
     home-manager
+    usbutils
+    pciutils
     vim
     wget
     htop
+    btop
     parted
     ddcutil
     fastfetch
+
+    conntrack-tools
+    socat
+    tcpdump
+    wireshark
+    wirelesstools
 
     fd
     plocate
     fzf
 
+    gnome-tweaks
     gnomeExtensions.appindicator
     vscode
     gh
@@ -182,45 +208,13 @@
     git
     telegram-desktop
     #rquickshare
+
+    protonup-qt
+    heroic
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
-
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
-  # to actually do that.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
   # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
   # and migrated your data accordingly.
-  #
   # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "25.11"; # Did you read the comment?
 
